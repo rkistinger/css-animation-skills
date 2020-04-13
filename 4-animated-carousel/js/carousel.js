5bf152f1fc148d65394e8221785e2cae2983c2d4
Ross Kistinger <rkistinger12@gmail.com>
2020-04-12 23:08:58 -0500

    4.4

--- 4-animated-carousel/js/carousel.js
+++ 4-animated-carousel/js/carousel.js
@@ -1,1 +1,34 @@
-window.addEventListener('load', () => {})
+window.addEventListener('load', () => {
+  function showNextQuote() {}
+
+  function showQuote() {}
+
+  function updateState(index) {}
+
+  function updateCarouselPosition() {}
+
+  function generatePips() {}
+
+  function updatePips() {}
+
+  function showFromPip() { }
+
+  function setLeftClass()
+
+  generatePips()
+
+  const prevIndex = 0
+  const currentIndex = 1
+  const nextIndex = 2
+  const lastIndex = document.querySelectorAll('.quote').length - 1
+  const carouselRunning = true
+  const interval = setInterval(() => {
+    if (carouselRunning) {
+      showNextQuote()
+    }
+  }, 4000)
+
+  document.querySelector('.quote.previous').addEventListener('click', showQuote)
+  document.querySelector('.quote.next').addEventListener('click', showQuote)
+  document.querySelector('.pip.next').addEventListener('click', showFromPip)
+})
